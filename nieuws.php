d6a1abebdaaf0d33a574c2fd007fa56a86601a86
Ivyk <ivyk96@live.com>
2014-11-17 18:02:25 +0100

    Nieuwsbrief geweizigd.

--- nieuws.php
+++ nieuws.php
@@ -51,17 +51,87 @@
             <div class="col-lg-12">
                 <h2 class="page-header">Nieuws</h2>
             </div>
-            <h4>21-03-1987</h4>
-            <p>Dit is gewoon even een test van hoe het eruit komt te zien als hier tekst zou staan.</p>
-            <h4>Oktoberfest!</h4>
-            <p>Dit is gewoon even een test van hoe het eruit komt te zien als hier tekst zou staan.</p>
-            <h4>Afvallen is nog nooit zo leuk geweest</h4>
+            <h4>Nieuwsbrief november 2014</h4>
             <p>
-                Dit is gewoon even een test van hoe het eruit komt te zien als hier tekst zou staan.
-                Dit is gewoon even een test van hoe het eruit komt te zien als hier tekst zou staan.
-                Dit is gewoon even een test van hoe het eruit komt te zien als hier tekst zou staan.
-                Dit is gewoon even een test van hoe het eruit komt te zien als hier tekst zou staan.
-                Dit is gewoon even een test van hoe het eruit komt te zien als hier tekst zou staan.
+            	Zoals altijd blijft Fit & Fun “in beweging” en gaat graag mee met de laatste nieuwe trends op 
+            	het gebied van bewegen en gezondheid, vandaar dat er vanaf 1 december weer een fantastische 
+            	nieuwe les bijkomt:
+            </p>
+            <p>INDOOR BOOTCAMP!!!</p>
+            <p>
+                Van oorsprong een militaire training en daarom hebben veel mensen het idee dat je door een 
+                commanderende trainer volledig wordt uitgeput! Nou.....dit is niet zo hoor! Nadat ik in 2011 
+                mijn Bootcamp Trainer Certificaat heb behaald, heb ik mij verdiept in een vorm van Bootcamp 
+                die voor een grote groep interessant is: jong, oud(er), man en vrouw!
+            </p>
+            <p>
+            	Het is een back-to-basic training geworden, dat wil zeggen dat je er geen apparaten bij 
+            	nodig hebt. Je maakt grotendeels gebruik van je eigen lichaamsgewicht om te verbeteren in 
+            	kracht en lenigheid, maar ook je uithoudingsvermogen gaat met sprongen vooruit. Deze 
+            	vooruitgang zullen we ook regelmatig testen, zodat iedereen zijn persoonlijk progressie 
+            	ook “ziet”. Iedereen kan op zijn eigen niveau meedoen, dus of je nu geoefend bent of een 
+            	beginnende sporter zonder conditie, dat maakt niet uit. Dat dit “indoor” (dus in de zaal) 
+            	is, is voor een hoop mensen prettig, zeker ’s winters! Geen last van regen, kou of blubber! 
+            	Deze les zal gegeven gaan worden vanaf 3 december op woensdagavond om 20.00 uur in De Willem.
+            </p>
+            <p>
+            	Dat brengt mij gelijk op het volgende: De woensdag avond is al geruime tijd helaas een
+            	stuk stiller dan voorheen. Dat heeft mij ertoe doen besluiten om de huidige lessen van
+            	deze avond vanaf woensdag 3 december te gaan vervangen door: 19.00 uur Club Yoga 20.00 uur 
+            	Indoor Bootcamp
+            </p>
+            <p>
+            	Er kwamen nogal wat verzoeken voor nog een avonduur Club Yoga (niet iedereen kan op 
+            	donderdagavond) en de ook Bootcamp is voor velen (mede omdat hier ook heren en jongelui
+            	 aan mee kunnen doen) een geschikte les!
+            </p>
+            <p>
+            	Wat mij dan weer brengt naar mijn (voorlopig) laatste mededeling:
+            </p>
+            <p>
+            	Vanaf december zullen de strippenkaarten niet meer persoonsgebonden zijn. Zeker ook omdat 
+            	de yoga en de bootcamp lessen zijn die ook voor andere gezinsleden geschikte lessen zijn 
+            	om te volgen. Ja kan dan dus met het hele gezin op één kaart. Je naam komt nog wel gewoon 
+            	op de kaart, maar dit om onduidelijkheid te voorkomen als je je kaart een keer in de 
+            	kleedkamer of zaal laat liggen.....
+            </p>
+            <p>
+            	De kaarten van 10 en 20 ritten blijven onbeperkt geldig, maar die van 30 ritten zal weer 
+            	een geldigheidsduur krijgen van 3 maanden. Het was voor mij de keuze: of de kaarten duurder 
+            	maken, of deze maatregel. Velen kochten nu nl de relatief “goedkope” 30 ritten kaart  
+            	waardoor mijn inkomsten omlaag gingen. Dit tezamen met de stijgende kosten van de (nieuwe) 
+            	zalen, was dan bijna niet rendabel meer, vandaar deze (wellicht impopulaire) maatregel.... 
+            	De vakanties komen nog steeds voor mijn rekening, dus mocht er een vakantie binnen die 3 
+            	maanden vallen, wordt de geldigheidsduur van de kaart verlengd met de duur van de vakantie.
+            </p>
+            <p>
+            	Nou ik hoop dat ik jullie toch nog een beetje blij heb gemaakt met de nieuwe lessen en de 
+            	frisse wind door Fit & Fun! Mocht je nog leuke suggesties hebben voor een les of iets 
+            	tegenkomen waarvan je denkt dat het leuk is om gezamenlijk aan deel te nemen: ik sta overal 
+            	voor open!
+            </p>
+            <p>
+            	Dan tot slot (en dan hoop ik jullie voorlopig even niet meer lastig te moeten vallen!): Op 
+            	zaterdag 7 februari wil ik met een team van Fit & Fun weer gaan meedoen met de 
+            	SPINNINGMARATHON ten bate van het Sophia Kinderziekenhuis! De marathon bestaat uit blokken 
+            	van 3 uur en ik wil starten om 09.00 uur (dus tot 12.00 uur). Je moet het echt een keer 
+            	meemaken, het is zo’n bijzonder evenement!! Je kan alle weerstand van je fiets halen, je kan 
+            	tussendoor gaan plassen, je laten masseren, men loopt rond met fruit en water, je familie kan 
+            	je komen aanmoedigen (gewoon naast je fiets komen staan!) maar je kan je ook helemaal lek 
+            	fietsen! Dit alles onder het genot van een live DJ en 274 andere fietsers!! Kortom, het is 
+            	voor iedereen leuk, goede conditie of niet! Voor meer informatie heb ik ook weer een bijlage 
+            	toegevoegd (pffff). Denk er (HEEL) even over na en geef je dan op bij mij, want de fietsen zijn 
+            	snel weg. Als je je hebt opgegeven en ik heb de fiets voor je gereserveerd, zit je vast aan de 
+            	verplichte reserverings- en sponsorkosten......want die moet ik voorschieten en krijg ik niet 
+            	terug van de organisatie als je toch niet komt.....
+            </p>
+            <p>
+            	Nou voorlopig is het wel weer even genoeg informatie hè! Mochten jullie nog vragen op 
+            	opmerkingen hebben, dan hoor ik het graag. Je mag me altijd bellen (078-6413 473 of 06-4169 
+            	6046), mailen of aanspreken voor of na de les!
+            </p>
+            <p>
+            	Sportieve Groeten van Jolanda
             </p>
         </div>
         <!-- /.row -->
